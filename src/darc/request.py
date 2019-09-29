4b572e5ee4e5a589d3dfd26d986825d00fa28869
Jarry XIAO <jarry.xiao@tek-id.com>
2019-09-29 10:52:52 +0800

    Minor update

--- src/darc/request.py
+++ src/darc/request.py
@@ -13,7 +13,12 @@ from darc.const import SOCKS_PORT
 #     return session
 
 
-def request(link: str) -> typing.Response:
-    """Request a link."""
+def get_nromal(link: str) -> typing.Response:
+    """Request a normal link."""
+    return requests.get(link)
+
+
+def get_tor(link: str) -> typing.Response:
+    """Request a Tor (.onion) link."""
     return requests.get(link, proxies={'http':  f'socks5://127.0.0.1:{SOCKS_PORT}',
                                        'https': f'socks5://127.0.0.1:{SOCKS_PORT}'})
