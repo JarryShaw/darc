ac51d6f685ba6f87bbd645a443beec8be4500015
Jarry Shaw <jarryshaw@icloud.com>
2020-03-28 10:48:08 +0800

    bugfix in uniq

--- extra/uniq.py
+++ extra/uniq.py
@@ -7,10 +7,11 @@ import tempfile
 
 
 def is_in(line, dest):
-    with open(dest) as file:
-        for content in filter(None, map(lambda s: s.strip(), file)):
-            if line == content:
-                return True
+    if os.path.isfile(dest):
+        with open(dest) as file:
+            for content in filter(None, map(lambda s: s.strip(), file)):
+                if line == content:
+                    return True
     return False
 
 
@@ -18,20 +19,20 @@ def uniq(path, tempdir):
     name = os.path.split(path)[1]
     dest = os.path.join(tempdir, '%s.tmp' % name)
 
-    with open(dest, 'w') as out_file:
-        with open(path) as file:
-            for line in filter(None, map(lambda s: s.strip(), file)):
-                if line.startswith('#'):
-                    continue
-                if is_in(line, dest):
-                    continue
+    with open(path) as file:
+        for line in filter(None, map(lambda s: s.strip(), file)):
+            if line.startswith('#'):
+                continue
+            if is_in(line, dest):
+                continue
+            with open(dest, 'at') as out_file:
                 print(line, file=out_file)
     os.rename(dest, path)
 
 
 def main():
     with tempfile.TemporaryDirectory() as tempdir:
-        for path in sys.argv[1]:
+        for path in sys.argv[1:]:
             uniq(path, tempdir)
 
 
