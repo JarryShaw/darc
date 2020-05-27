8eb3610192f4bb8fbdbfc342fc2f61dd582d5a76
Jarry Shaw <jarryshaw@icloud.com>
2020-05-27 22:13:33 +0800

    revised  upload

--- extra/upload.sh
+++ extra/upload.sh
@@ -2,17 +2,26 @@
 
 set -ex
 
+function retry() {
+    while true; do
+        >&2 echo "+ $@"
+        $@ && break
+        >&2 echo "exit: $?"
+    done
+    >&2 echo "exit: 0"
+}
+
 # archive filename
 filename="darc-api-$(hostname -I | awk '{print $1}')-$(date +%Y%m%d-%H%M%S).tar.gz"
 
 # compare API files
-tar -czf ${filename} api
+retry tar -czf ${filename} api
 rm -r api
 
 # upload archive
 if [ -z ${USER} ]; then
-    curl -T ${filename} ${HOST}
+    retry curl -T ${filename} ${HOST}
 else
-    curl -T ${filename} ${HOST} --user ${USER}
+    retry curl -T ${filename} ${HOST} --user ${USER}
 fi
 rm ${filename}
